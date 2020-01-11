3dad48c85e26f4d664407d0b271cf5faa02fa504
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-11 21:40:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ end
 
 def introduction_with_language(name, language)
   name = "Dan" 
-  language = "Ember.js"
+  language = "Ember.js.\n"
  puts "Hi, my name is #{name} and I am learning to program in {language}."
 end
 
